ccc5d9e9cea51a4e0a55cc09eaee4728c311fac3
Maham Saeed <mahamsaeed411@gmail.com>
2026-06-20 17:35:15 +0100

    Implemented task editing feature

--- index.js
+++ index.js
@@ -11,12 +11,13 @@ let taskList = document.getElementById("task-list")
 //added delete option
 addButton.addEventListener ("click", function() {
 
-    let value = input.value
+    let text = document.createElement("span")
+    text.innerHTML = input.value
     let li = document.createElement("li")
 
     let checkbox = document.createElement("input")
     checkbox.type = "checkbox"
-    let deleteItem = document.createElement("svg")
+    let deleteItem = document.createElement("span")
 
     deleteItem.innerHTML = `
     <svg id="delete" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 30 30">
@@ -28,7 +29,31 @@ addButton.addEventListener ("click", function() {
         li.remove()
     })
 
-    li.append(checkbox, value, deleteItem)
+    let edit = document.createElement("span")
+    edit.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+    <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
+    </svg>
+    `
+
+    edit.addEventListener("click", function() {
+
+        let inputEdit = document.createElement("input")
+        inputEdit.type = "text"
+        inputEdit.value = text.innerHTML
+
+        li.replaceChild(inputEdit, text)
+        inputEdit.focus()
+
+        inputEdit.addEventListener("keydown", function(e) {
+            if(e.key === "Enter") {
+                text.innerHTML = inputEdit.value
+                li.replaceChild(text, inputEdit)
+            }
+        })
+
+    })
+    li.append(checkbox, text, edit, deleteItem)
     taskList.append(li)
     input.value = ""
 })
